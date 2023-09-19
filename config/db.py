851aba8709b607d3e6d2574d443b1d19a0cdc26f
dietgar <dev.dietmar@gmail.com>
2023-09-19 11:34:13 -0600

    Version 1.0 de la API

--- config/db.py
+++ config/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 
 engine = create_engine(
-    "mysql+pymysql://root:Rotting7-Hankie3@localhost:3306/system_health_mobile")
+    "mysql+pymysql://root:password@localhost:3306/db_name")
 
 meta = MetaData()
